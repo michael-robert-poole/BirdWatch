4089488010f330046165eb9c6949cd1aee3ce536
mike-rob-poole <mike.rob.poole@gmail.com>
2021-12-07 17:46:07 +0000

    create bird dictionary

--- bird_info.py
+++ bird_info.py
@@ -3,24 +3,18 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import const
 
-
-COMMON_NAME=["Wood Pigeon", "Long-Tailed Tit",
-                 "Eurasian Magpie","European Goldfinch","European Robin","Eurasian Blue Tit","Great Tit","House Sparrow","Common Starling","Common Blackbird"]
-LATIN_NAME=["Columba palumbus", "Aegithalos caudatus", "Pica pica", "Carduelis carduelis", "Erithacus_rubecula", "Cyanistes caeruleus", "Parus major", "Passer_domesticus"
-                , "Sturnus vulgaris", "Turdus merula"]
    
 def visualise_images(images, labels, latin=False):
-    names = LATIN_NAME if latin else COMMON_NAME
-    print(len(images))
+    common_name = 0 if latin else 1
     indexes=np.arange(start=0, stop=len(images)-2, step=math.ceil(len(images)/100))
-    print(labels[indexes[99]])
     plt.figure(figsize=(30, 30))
     j=0
     for i in indexes:
         ax = plt.subplot(10, 10, j + 1)
         plt.imshow(images[i])
-        plt.title(names[labels[i]] + str(images[i].shape))
+        plt.title(const.BIRDS_DICT[labels[i]][common_name] + str(images[i].shape))
         plt.axis('off')
         j+=1
             
---
+++ const.py
@@ -0,0 +1,13 @@
+BIRDS_DICT={
+    0:["Turdus merula", "Common Blackbird"],
+    1:["Passer domesticus", "House Sparrow"],
+    2:["Parus major", "Great Tit"],
+    3:["Columba palumbus", "Wood Pigeon"],
+    4:["Erithacus rubecula", "European Robin"],
+    5:["Pica_pica", "Eurasian Magpie"],
+    6:["Cyanistes caeruleus", "Eurasian Blue Tit"],
+    7:["Sturnus vulgaris", "Common Starling"],
+    8:["Carduelis carduelis", "European Goldfinch"],
+    9:["Aegithalos_caudatus", "Long-Tailed Tit"]
+}
+
