6f0fcc71cefaac86b4ecfefb2c9d32d90dccf6d9
michael-robert-poole <81023070+michael-robert-poole@users.noreply.github.com>
2021-12-07 15:31:03 +0000

    BW-004 (#6)
    * load as numpy arrays and assign image quality

--- bird_info.py
+++ bird_info.py
@@ -2,24 +2,25 @@ import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
-class Bird_info:
-    
-    COMMON_NAME=["Wood Pigeon", "Long-Tailed Tit",
+
+COMMON_NAME=["Wood Pigeon", "Long-Tailed Tit",
                  "Eurasian Magpie","European Goldfinch","European Robin","Eurasian Blue Tit","Great Tit","House Sparrow","Common Starling","Common Blackbird"]
-    LATIN_NAME=["Columba palumbus", "Aegithalos caudatus", "Pica pica", "Carduelis carduelis", "Erithacus_rubecula", "Cyanistes caeruleus", "Parus major", "Passer_domesticus"
+LATIN_NAME=["Columba palumbus", "Aegithalos caudatus", "Pica pica", "Carduelis carduelis", "Erithacus_rubecula", "Cyanistes caeruleus", "Parus major", "Passer_domesticus"
                 , "Sturnus vulgaris", "Turdus merula"]
-
-    
    
-    def visualise_images(self, data, latin=False):
-        names = self.LATIN_NAME if latin else self.COMMON_NAME
-        
-        plt.figure(figsize=(10, 10))
-        for images, categories in data.take(1):
-            for i in range(9):
-                ax = plt.subplot(3, 3, i + 1)
-                plt.imshow(images[i].numpy().astype("uint8"))
-                class_index = np.where(categories[i].numpy()==1)
-                plt.title(names[int(class_index[0])])
-                plt.axis("off")
+def visualise_images(images, labels, latin=False):
+    names = LATIN_NAME if latin else COMMON_NAME
+    print(len(images))
+    indexes=np.arange(start=0, stop=len(images)-2, step=math.ceil(len(images)/100))
+    print(labels[indexes[99]])
+    plt.figure(figsize=(30, 30))
+    j=0
+    for i in indexes:
+        ax = plt.subplot(10, 10, j + 1)
+        plt.imshow(images[i])
+        plt.title(names[labels[i]] + str(images[i].shape))
+        plt.axis('off')
+        j+=1
+            
