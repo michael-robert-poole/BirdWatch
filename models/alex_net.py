c751d4016a7b599a067a73a7c4d02c7284b0b3cb
mike-rob-poole <mike.rob.poole@gmail.com>
2021-12-12 15:43:27 +0000

    very initial commit

---
+++ models/alex_net.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Activation, Flatten, Dense, Dropout
+
+def alexNet():
+    #Instantiation
+    AlexNet = keras.Sequential()
+    
+    AlexNet.add(layers.RandomFlip("horizontal"))
+    AlexNet.add(layers.RandomRotation(0.1))
+
+    #1st Convolutional Layer - 96 filters instead of two parllel 48 filters
+    AlexNet.add(Conv2D(filters=96, input_shape=(256, 256,3), kernel_size=(3,3), strides=(4,4), padding='same'))
+    AlexNet.add(BatchNormalization())
+    AlexNet.add(Activation('relu'))
+    AlexNet.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'))
+
+    #2nd Convolutional Layer
+    AlexNet.add(Conv2D(filters=256, kernel_size=(5, 5), strides=(1,1), padding='same'))
+    AlexNet.add(BatchNormalization())
+    AlexNet.add(Activation('relu'))
+    AlexNet.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'))
+
+    #3rd Convolutional Layer
+    AlexNet.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same'))
+    AlexNet.add(BatchNormalization())
+    AlexNet.add(Activation('relu'))
+
+    #4th Convolutional Layer
+    AlexNet.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same'))
+    AlexNet.add(BatchNormalization())
+    AlexNet.add(Activation('relu'))
+
+    #5th Convolutional Layer
+    AlexNet.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='same'))
+    AlexNet.add(BatchNormalization())
+    AlexNet.add(Activation('relu'))
+    AlexNet.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'))
+
+    #Passing it to a Fully Connected layer
+    AlexNet.add(Flatten())
+    # 1st Fully Connected Layer
+    AlexNet.add(Dense(4096, input_shape=(32,32,3,)))
+    AlexNet.add(BatchNormalization())
+    AlexNet.add(Activation('relu'))
+    # Add Dropout to prevent overfitting
+    AlexNet.add(Dropout(0.4))
+
+    #2nd Fully Connected Layer
+    AlexNet.add(Dense(4096))
+    AlexNet.add(BatchNormalization())
+    AlexNet.add(Activation('relu'))
+    #Add Dropout
+    AlexNet.add(Dropout(0.4))
+
+    #3rd Fully Connected Layer
+    AlexNet.add(Dense(1000))
+    AlexNet.add(BatchNormalization())
+    AlexNet.add(Activation('relu'))
+    #Add Dropout
+    AlexNet.add(Dropout(0.4))
+
+    #Output Layer
+    AlexNet.add(Dense(10))
+    AlexNet.add(BatchNormalization())
+    AlexNet.add(Activation('softmax'))
+    
+    return AlexNet
