a17c720e5c36ce6c9027c6b9ce186a38b52df81a
mike-rob-poole <mike.rob.poole@gmail.com>
2021-12-14 12:54:30 +0000

    pre-proces test numpy array

--- py_files/bird_info.py
+++ py_files/bird_info.py
@@ -3,18 +3,19 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-import const
+import py_files
+from py_files import const
 
    
-def visualise_images(images, labels, latin=False):
+def visualise_10_10_images(data, latin=False):
     common_name = 0 if latin else 1
-    indexes=np.arange(start=0, stop=len(images)-2, step=math.ceil(len(images)/100))
+    indexes=np.arange(start=0, stop=len(data), step=math.ceil(len(data)/100))
     plt.figure(figsize=(30, 30))
     j=0
     for i in indexes:
         ax = plt.subplot(10, 10, j + 1)
-        plt.imshow(images[i])
-        plt.title(const.BIRDS_DICT[labels[i]][common_name])
+        plt.imshow(data[i,0])
+        plt.title(const.BIRDS_DICT[data[i,1]][0])
         plt.axis('off')
         j+=1
             
