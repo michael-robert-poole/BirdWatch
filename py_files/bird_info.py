2ad06f219f4e8c757cb13ba1ecd2370d30efe76d
mike-rob-poole <mike.rob.poole@gmail.com>
2021-12-15 12:48:33 +0000

    update visualise 10X10 bird method

--- py_files/bird_info.py
+++ py_files/bird_info.py
@@ -9,13 +9,12 @@ from py_files import const
    
 def visualise_10_10_images(data, latin=False):
     common_name = 0 if latin else 1
-    indexes=np.arange(start=0, stop=len(data), step=math.ceil(len(data)/100))
+    random_indexes=[np.random.choice((np.where(data[:,1]==label)[0]), 10) for label in labels_value]
     plt.figure(figsize=(30, 30))
-    j=0
-    for i in indexes:
-        ax = plt.subplot(10, 10, j + 1)
-        plt.imshow(data[i,0])
-        plt.title(const.BIRDS_DICT[data[i,1]][0])
+    for i, r_index in enumerate(random_indexes):
+        ax = plt.subplot(10, 10, i + 1)
+        plt.imshow(data[r_index,0])
+        plt.title(const.BIRDS_DICT[data[r_index,1]][common_name])
         plt.axis('off')
-        j+=1
+      
             
