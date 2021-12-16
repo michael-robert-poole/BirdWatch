2a2371541f01fb20db1211f375ad4e9f22ec5cf3
mike-rob-poole <mike.rob.poole@gmail.com>
2021-12-16 10:38:31 +0000

    tidy up

--- py_files/bird_info.py
+++ py_files/bird_info.py
@@ -7,7 +7,7 @@ import py_files
 from py_files import const
 
    
-def visualise_10_10_images(data, latin=False):
+def visualise_10_10_images(data, labels_value, latin=False):
     common_name = 0 if latin else 1
     random_indexes=[np.random.choice((np.where(data[:,1]==label)[0]), 10) for label in labels_value]
     random_indexes=np.array(random_indexes).reshape(100)
