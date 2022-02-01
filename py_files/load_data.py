4b15d176f217ac6f50bb46aa2d8f028cf4b5ae96
mike-rob-poole <mike.rob.poole@gmail.com>
2022-02-01 13:57:19 +0000

    update util files

--- py_files/load_data.py
+++ py_files/load_data.py
@@ -1,43 +1,1 @@
-import tensorflow as tf
-from tensorflow import keras
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import cv2
-
-DIRECTORY="birds"
-IMG_SIZE=[256,256]
-VAL_SPLIT=0.1
-SEED=133
-   
-
-def split_data(subset, label_mode,labels):
-    dataset=keras.preprocessing.image_dataset_from_directory(
-        directory=DIRECTORY,
-        image_size=IMG_SIZE,
-        validation_split=VAL_SPLIT,
-        seed=SEED,
-        subset=subset,
-        label_mode= label_mode,
-        labels=labels,
-        batch_size=16
-  )
-    return dataset
-
-def load_data_from_folder(parent_dir):
-    images = []
-    labels = []
-    label = 0
-
-    parent_folder = os.listdir(parent_dir)
-
-    for f in parent_folder:
-        print(f)
-        sub_folder=os.listdir(parent_dir+"/"+f)
-        for image in sub_folder:
-            theImage=cv2.imread(parent_dir+"/"+f+"/"+image, cv2.IMREAD_COLOR)
-            im_rgb = cv2.cvtColor(theImage, cv2.COLOR_BGR2RGB)
-            images.append(im_rgb)
-            labels.append(label)
-        label=label+1
-    return images, labels
+import tensorflow as tffrom tensorflow import kerasimport matplotlib.pyplot as pltimport numpy as npimport osimport cv2DIRECTORY="birds"IMG_SIZE=[256,256]VAL_SPLIT=0.1SEED=133   def split_data(subset, label_mode,labels):    dataset=keras.preprocessing.image_dataset_from_directory(        directory=DIRECTORY,        image_size=IMG_SIZE,        validation_split=VAL_SPLIT,        seed=SEED,        subset=subset,        label_mode= label_mode,        labels=labels,        batch_size=16  )    return datasetdef load_data_from_folder(parent_dir):    images = []    labels = []    label = 0    parent_folder = os.listdir(parent_dir)    parent_folder.sort()        for f in parent_folder:        print(f)        sub_folder=os.listdir(parent_dir+"/"+f)        for image in sub_folder:            theImage=cv2.imread(parent_dir+"/"+f+"/"+image, cv2.IMREAD_COLOR)            im_rgb = cv2.cvtColor(theImage, cv2.COLOR_BGR2RGB)            images.append(im_rgb)            labels.append(label)        label=label+1    return images, labels
